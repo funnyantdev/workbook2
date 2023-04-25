643c2d01773298413f1ed5cac4a578e87c6b19e0
Craig McKeachie <craig@funnyant.com>
2023-04-25 11:06:19 -0400

    initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var firstName, lastName;
+firstName = "Jon";
+lastName = "Tran";
+var dateOfBirth;
+var gender;
+var autoPolicyNumber;
+
+console.log('"HI! My name is: "' + firstName + lastName);
