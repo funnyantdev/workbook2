6fdd8b3bfea1ce881749e85d6a6945e1cf82de5f
Craig McKeachie <craig@funnyant.com>
2023-04-25 13:17:51 -0400

    object intro

---
+++ exercises/net_worth.js
@@ -0,0 +1,15 @@
+// var assets = 100000;
+// var debts = 20000;
+
+// var obj = new Object();
+// obj.assets = 100000;
+// obj.debts = 20000;
+var obj = { assets: 100000, debts: 20000 }
+var MyMath = {PI: 3.14157, round: function(){}}
+
+var netWorth = obj.assets - obj.debts;
+console.log("Your net worth is " + netWorth);
+console.log(Math.PI);
+console.log(Math.round(12.126));
+
+
