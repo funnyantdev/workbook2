02f54c1f7c3c5e8641f1658e371a676f6ac91aff
Craig McKeachie <craig@funnyant.com>
2023-04-26 10:19:01 -0400

    add parseFloat

--- demos/parse_string_to_number.js
+++ demos/parse_string_to_number.js
@@ -1,11 +1,29 @@
-"use strict"
+"use strict";
 
+//parseInt
 //declaring variable a and assigning a string to it
-var a = "100";
+var a = " 100 " ;
 var b = parseInt(a);
+console.log("b is " + b);
 
-var dataTypeOfAVariable = typeof(a);
+var dataTypeOfAVariable = typeof a;
 console.log(dataTypeOfAVariable);
 
-var dataTypeOfBVariable = typeof(b);
+var dataTypeOfBVariable = typeof b;
 console.log(dataTypeOfBVariable);
+
+
+//parseFloat
+var c = "100.13" ;
+var d = parseFloat(c);
+console.log("d is " + d);
+
+var dataTypeOfCVariable = typeof c;
+console.log(dataTypeOfCVariable);
+
+var dataTypeOfDVariable = typeof d;
+console.log(dataTypeOfDVariable);
+
+
+
+
