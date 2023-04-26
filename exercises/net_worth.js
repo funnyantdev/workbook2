cbd6c3591fbf711037f466b2b7f1f460599f52d8
Craig McKeachie <craig@funnyant.com>
2023-04-26 08:47:07 -0400

    net worth with object

--- exercises/net_worth.js
+++ exercises/net_worth.js
@@ -4,12 +4,12 @@
 // var obj = new Object();
 // obj.assets = 100000;
 // obj.debts = 20000;
-var obj = { assets: 100000, debts: 20000 }
-var MyMath = {PI: 3.14157, round: function(){}}
+var balanceSheetForFunnyAnt = { assets: 400000, debts: 20000 };
+var balanceSheetForGreatLiving = { assets: 100000, debts: 20000 };
+balanceSheetForFunnyAnt.assets
+var MyMath = { PI: 3.14157, round: function () {} };
 
 var netWorth = obj.assets - obj.debts;
 console.log("Your net worth is " + netWorth);
 console.log(Math.PI);
-console.log(Math.round(12.126));
-
-
+console.log(Math.cos(12.126));
