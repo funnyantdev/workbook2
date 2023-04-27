86a122fa13fdcaba6361426b8be0f11b795d177a
Craig McKeachie <craig@funnyant.com>
2023-04-27 14:45:56 -0400

    template literal

---
+++ demos/template-literal.js
@@ -0,0 +1,12 @@
+let first = "a";
+let second = "b";
+let third = "c";
+
+let alphabet = first + second + third;
+console.log(alphabet);
+
+alphabet = "Now I know my " + first + ", " + second + ", " + third + "'s";
+console.log(alphabet);
+
+alphabet = `Now I know my ${first}, ${second}, ${third}'s  `;
+console.log(alphabet);
